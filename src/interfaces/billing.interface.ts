f9b291a2aa47327391913cb10cf5726f3f5819ea
Utech17 <127623759+Utech17@users.noreply.github.com>
2024-12-12 18:46:40 -0400

    interface

--- src/interfaces/billing.interface.ts
+++ src/interfaces/billing.interface.ts
@@ -7,6 +7,9 @@ export interface BillingInterface {
   billing_date: Date;
   billing_status?: "pending" | "paid" | "cancelled";
   client_id: number | string;
+  createdAt:Date;
+  updatedAt:Date;
+  deletedAt:Date;
   Client?:ClientInterface;
   Patient?:PatientInterface;
 }
