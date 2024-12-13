bab24776020839bc7ed19f56491f0c610cd2b381
Richard <richardcortez934@gmail.com>
2024-12-12 23:22:07 -0400

    Create event.details.data

---
+++ src/data/event.details.data.ts
@@ -0,0 +1,32 @@
+export const eventDetailsData = [
+    {
+        id: 1,
+        id_events: 1,
+        id_actions: 1,
+        detail_value: 37,
+    },
+    {
+        id: 2,
+        id_events: 2,
+        id_actions: 2,
+        detail_value: 5,
+    },
+    {
+        id: 3,
+        id_events: 3,
+        id_actions: 4,
+        detail_value: 10,
+    },
+    {
+        id: 4,
+        id_events: 4,
+        id_actions: 3,
+        detail_value: 1,
+    },
+    {
+        id: 5,
+        id_events: 5,
+        id_actions: 5,
+        detail_value: 15,
+    },
+]
