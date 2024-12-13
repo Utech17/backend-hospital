b6edec9bbfdc5cb8971693f68500989086e0207c
richardcort <144558240+richardcort@users.noreply.github.com>
2024-12-12 23:14:50 -0400

    Create event.types.data

---
+++ src/data/event.types.data.ts
@@ -0,0 +1,22 @@
+export const eventTypesData = [
+    {
+        id: 1,
+        event_name: 'Medical Consultation',
+    },
+    {
+        id: 2,
+        event_name: 'Surgery',
+    },
+    {
+        id: 3,
+        event_name: 'Hospitalization',
+    },
+    {
+        id: 4,
+        event_name: 'Diagnostic Procedure',
+    },
+    {
+        id: 5,
+        event_name: 'Therapy',
+    },
+]
