bed1a59205529687916aca377b8cd37a5c5e93fa
Utech17 <127623759+Utech17@users.noreply.github.com>
2024-12-14 01:19:55 -0400

    seeders 2 mas

--- src/data/seeders/user.seed.ts
+++ src/data/seeders/user.seed.ts
@@ -31,6 +31,26 @@ const usersSeeds: Partial<UserInterface>[] = [
     isActive: false,
     roleId: 3,
   },
+  {
+    id: 4,
+    firstName: "Debora",
+    lastName: "Mayurel",
+    email: "debora.mayurel@example.com",
+    password: "securePasswordDebora",
+    createdAt: new Date("2024-04-01T11:00:00Z"),
+    isActive: true,
+    roleId: 2,
+  },
+  {
+    id: 5,
+    firstName: "Leticia",
+    lastName: "Parra",
+    email: "leticia.parra@example.com",
+    password: "securePasswordLeticia",
+    createdAt: new Date("2024-05-01T12:00:00Z"),
+    isActive: true,
+    roleId: 1,
+  },
 ];
 
 export { usersSeeds };
