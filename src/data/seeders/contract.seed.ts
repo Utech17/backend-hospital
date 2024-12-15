99a5d9e700890715f7e4bb9d1e9ec961f8f6c047
Ellyhan Rodriguez <ellyhanrodriguez@gmail.com>
2024-12-15 15:13:37 -0400

    creacion de seed

---
+++ src/data/seeders/contract.seed.ts
@@ -0,0 +1,34 @@
+export const contractData =  [
+    {
+        id: 1,
+        id_employee: 26737737,
+        id_workingDay: 1,
+        id_charge: 1,
+        start_date: '2024-12-12T12:00:00-04:00',
+        end_date: '2025-12-12T12:00:00-04:00',
+        base_salary: 1500.00,
+        benefits: 'none',
+    },
+
+    {
+        id: 2,
+        id_employee: 5609825,
+        id_workingDay: 2,
+        id_charge: 2,
+        start_date: '2024-10-10T12:00:00-04:00',
+        end_date: '2025-10-10T12:00:00-04:00',
+        base_salary: 2312.78,
+        benefits: 'seguro de vida',
+    },
+
+    {
+        id: 3,
+        id_employee: 6244046,
+        id_workingDay: 1,
+        id_charge: 3,
+        start_date: '2022-08-08T12:00:00-04:00',
+        end_date: '2024-10-08T12:00:00-04:00',
+        base_salary: 3520.12,
+        benefits: 'bono de transporte y de alimentacion',
+    },
+];
