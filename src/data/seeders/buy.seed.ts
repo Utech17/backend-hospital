352aeaabd606042146edf5d0ab9d680fa8a4f79e
Utech17 <127623759+Utech17@users.noreply.github.com>
2024-12-15 13:04:17 -0400

    seeders

---
+++ src/data/seeders/buy.seed.ts
@@ -0,0 +1,49 @@
+import { BuyInterface } from "../../interfaces";
+
+const buySeeds: Partial<BuyInterface>[] = [
+    
+  {
+    id: 1,
+    invoice_number: 1001,
+    date: new Date("2024-12-13T00:00:00Z"),
+    amount: 100.00,
+    status: true,
+    createdAt: new Date("2024-12-13T00:00:00Z"),
+    updatedAt: new Date("2024-12-13T00:00:00Z"),
+    deletedAt: null,
+  },
+  {
+    id: 2,
+    invoice_number: 1002,
+    date: new Date("2024-12-13T00:00:00Z"),
+    amount: 200.00,
+    status: false,
+    createdAt: new Date("2024-12-13T00:00:00Z"),
+    updatedAt: new Date("2024-12-13T00:00:00Z"),
+    deletedAt: null,
+  },
+  {
+    id: 3,
+    invoice_number: 1003,
+    date: new Date("2024-12-13T00:00:00Z"),
+    amount: 300.00,
+    status: true,
+    createdAt: new Date("2024-12-13T00:00:00Z"),
+    updatedAt: new Date("2024-12-13T00:00:00Z"),
+    deletedAt: null,
+  },
+  {
+    id: 4,
+    invoice_number: 1004,
+    date: new Date("2024-12-13T00:00:00Z"),
+    amount: 400.00,
+    status: true,
+    createdAt: new Date("2024-12-13T00:00:00Z"),
+    updatedAt: new Date("2024-12-13T00:00:00Z"),
+    deletedAt: null,
+  },
+];
+
+export {
+  buySeeds,
+};
