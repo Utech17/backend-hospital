d8e0fc3cd31012e904c2f49734060e21799866fd
Utech17 <127623759+Utech17@users.noreply.github.com>
2024-12-15 18:37:34 -0400

    ajuste parte 5

--- src/data/seeders/organizational.units.seed.ts
+++ src/data/seeders/organizational.units.seed.ts
@@ -1,29 +1,27 @@
-import { organizational_unitsInterface } from "../../interfaces";
+import { OrganizationalUnitsInterface } from "../../interfaces";
 
-const organizational_unitsInterface: Partial<organizational_unitsInterface>[] = [
+const organizationalUnitsInterface: Partial<OrganizationalUnitsInterface>[] = [
    {   
-    id?:"1";
-    units_name: "Material de oficina";
-    units_type: "Consumo administrativo";
-    id_departament: "1";
-    location: "Almacen 1"; 
-    Departament?:"Administracion";
+    id:"1",
+    units_name: "Material de oficina",
+    units_type: "Consumo administrativo",
+    id_departament: 1,
+    location: "Almacen 1",
 },
     {   
-    id?:"2";
-    units_name: "Insumos Laboratorio";
-    units_type: "Materiales de laboratorio"; 
-    id_departament: "2";
-    location:  "Almacen 2"; 
-    Departament?:"Laboratorio";
+    id:"2",
+    units_name: "Insumos Laboratorio",
+    units_type: "Materiales de laboratorio",
+    id_departament: 2,
+    location:  "Almacen 2",
 }, 
     {   
-    id?:"3";
-    units_name: "Inventario compras";
-    units_type: "Mercancia";
-    id_departament: "3";
-    location: "Almacen 3"; 
-    Departament?:"Compras";
+    id:"3",
+    units_name: "Inventario compras",
+    units_type: "Mercancia",
+    id_departament: 3,
+    location: "Almacen 3",
 },
 ];
-export {organizational_unitsInterface};
+
+export {organizationalUnitsInterface};
