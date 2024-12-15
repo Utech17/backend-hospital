d85f7505c4b7a716605e28abc28814a8f3523b93
Maikeljpl <maikeljpl.ks@gmail.com>
2024-12-14 22:48:34 -0400

    Commit seeders charge

---
+++ src/data/seeders/charge.data.ts
@@ -0,0 +1,83 @@
+import { ChargeInterface } from "../../interfaces";
+
+const chargesSeeds: Partial<ChargeInterface>[] = [
+    {
+        id: 1,
+        charge_name: "Médico de Base",
+        description: "Médico experimentado",
+      },
+      {
+        id: 2,
+        charge_name: "Residente",
+        description: "Médico en práctica",
+      },
+      {
+        id: 3,
+        charge_name: "Enfermera",
+        description: "Profesional de la salud que se encarga del cuidado de los pacientes",
+      },
+      {
+        id: 4,
+        charge_name: "Técnico de Laboratorio",
+        description: "Profesional que se encarga de realizar análisis y pruebas en el laboratorio",
+      },
+      {
+        id: 5,
+        charge_name: "Fisioterapeuta",
+        description: "Profesional que se encarga de la rehabilitación y tratamiento de lesiones y dolencias",
+      },
+      {
+        id: 6,
+        charge_name: "Terapeuta Ocupacional",
+        description: "Profesional que se encarga de la rehabilitación y tratamiento de lesiones y dolencias",
+      },
+      {
+        id: 7,
+        charge_name: "Psicólogo",
+        description: "Profesional que se encarga de la salud mental y emocional de los pacientes",
+      },
+      {
+        id: 8,
+        charge_name: "Radiólogo",
+        description: "Profesional que se encarga de la interpretación de imágenes médicas",
+      },
+      {
+        id: 9,
+        charge_name: "Anestesista",
+        description: "Profesional que se encarga de la administración de anestesia",
+      },
+      {
+        id: 10,
+        charge_name: "Cirujano",
+        description: "Profesional que se encarga de la realización de cirugías",
+      },
+      {
+        id: 11,
+        charge_name: "Pediatra",
+        description: "Médico especializado en el cuidado de los niños",
+      },
+      {
+        id: 12,
+        charge_name: "Ginecólogo",
+        description: "Médico especializado en el cuidado de la salud de la mujer",
+      },
+      {
+        id: 13,
+        charge_name: "Cardiólogo",
+        description: "Médico especializado en el cuidado del corazón",
+      },
+      {
+        id: 14,
+        charge_name: "Neumólogo",
+        description: "Médico especializado en el cuidado de los pulmones",
+      },
+      {
+        id: 15,
+        charge_name: "Oncólogo",
+        description: "Médico especializado en el cuidado del cáncer",
+      },
+    ];
+    
+    export{
+      chargesSeeds
+    }
