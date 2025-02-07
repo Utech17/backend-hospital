b9daebca34ea9991c28954a44730ff08a148bdc6
Nikku <yoks.rar@gmail.com>
2025-02-07 03:54:22 -0400

    Se arreglaron unos errores del route y del service, y se añadió la documentación de swagger en un archivo yml

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -2,46 +2,46 @@ import { RoleDB } from "../config";
 import { RoleInterface } from "../interfaces";
 
 const RoleService = {
-    getAll: async () => {
-        try {
-            const Roles = await RoleDB.findAll({
-                where: {
-                    status: true,
-                },
-            });
+  getAll: async () => {
+    try {
+      const Roles = await RoleDB.findAll({
+        where: {
 
-            if (Roles.length === 0) {
-                return {
-                    message: `No records found`,
-                    status: 404,
-                    data: {
-                        Roles,
-                    },
-                };
-            }
+        },
+      });
 
-            return {
-                message: `Records found`,
-                status: 200,
-                data: {
-                },
-            };
+      if (Roles.length === 0) {
+        return {
+          message: `No records found`,
+          status: 404,
+          data: {
+            Roles,
+          },
+        };
+      }
 
-        } catch (error) {
-            console.error('Error fetching role:', error);
-            return {
-                message: 'Error fetching role',
-                status: 500,
-            };
-        }
-    },
+      return {
+        message: `Records found`,
+        status: 200,
+        data: {
+          Roles,
+        },
+      };
 
-getOne: async (id: number | string) => {
+    } catch (error) {
+      console.error('Error fetching role:', error);
+      return {
+        message: 'Error fetching role',
+        status: 500,
+      };
+    }
+  },
+
+  getOne: async (id: number | string) => {
     try {
       const Role = await RoleDB.findOne({
         where: {
-          id,
-          status: true,
+          id
         },
       })
 
@@ -109,25 +109,56 @@ getOne: async (id: number | string) => {
     }
   },
 
+  // Eliminación lógica: Actualiza el campo status a false
+  // delete: async (id: number | string) => {
+  //   try {
+  //     await RoleDB.update(
+  //       {
+  //         status: false,
+  //         deletedAt: new Date(),
+  //       },
+  //       { where: { id } }
+  //     )
+  //     return {
+  //       message: "Successful removal",
+  //       status: 204,
+  //       data: {},
+  //     }
+  //   } catch (error) {
+  //     return {
+  //       message: "Error deleting role",
+  //       status: 500,
+  //     }
+  //   }
+  // },
+
+  // Eliminación física: Elimina el registro de la base de datos
   delete: async (id: number | string) => {
     try {
-      await RoleDB.update(
-        {
-          status: false,
-          deletedAt: new Date(),
+      const result = await RoleDB.destroy({
+        where: {
+          id,
         },
-        { where: { id } }
-      )
-      return {
-        message: "Successful removal",
-        status: 204,
-        data: {},
+      });
+  
+      if (result === 0) {
+        return {
+          message: "Record not found",
+          status: 404,
+          data: {},
+        };
+      } else {
+        return {
+          message: "Successful removal",
+          status: 204,
+          data: {},
+        };
       }
     } catch (error) {
       return {
-        message: "Error fetching role",
+        message: "Error deleting role",
         status: 500,
-      }
+      };
     }
   },
 
