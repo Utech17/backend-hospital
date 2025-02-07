1bea0bd5fa0284150a6bbe571c5456d46b87593b
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-02-07 00:57:05 -0400

    prueba

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,4 +1,4 @@
-const port = process.env.API_PORT || 3900;
+const port = process.env.PORT || 3900;
 const pre = "/api";
 const swaggerOptions = {
   swaggerDefinition: {
--- src/server/server.ts
+++ src/server/server.ts
@@ -55,7 +55,7 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.API_PORT || 3900;
+    this.port = process.env.PORT || 3900;
     this.pre = "/api";
     this.paths = {
       Action: this.pre + "/Action",
