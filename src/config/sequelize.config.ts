56e895414318d3e353cd1877363a0b944ba0743a
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-02-07 01:38:59 -0400

    xd

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -59,7 +59,7 @@ const dbPassword: string | undefined = process.env.DATABASE_PASSWORD
 // Instanciamos el objeto Sequelize
 const db = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: "mysql",
-  host: "https://backend-hospital-skii.onrender.com",
+  host: "hospital-backend-hospital.h.aivencloud.com",
   logging: false,
 });
 
