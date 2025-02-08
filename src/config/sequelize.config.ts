080416310eeac5168c939793d6b070c8a15e2ff4
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-02-07 20:56:49 -0400

    Revert "xd 2"
    This reverts commit e119996b8716ffa8c47999d98ab35ac5ab94d0d5.

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -60,10 +60,7 @@ const dbPassword: string | undefined = process.env.DATABASE_PASSWORD
 const db = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: "mysql",
   host: "hospital-backend-hospital.h.aivencloud.com",
-  logging: console.log, // Habilita el registro de consultas SQL y mensajes de depuración
-  dialectOptions: {
-    connectTimeout: 60000, // Aumenta el tiempo de espera de la conexión a 60 segundos
-  },
+  logging: false,
 });
 
 const Options = {
