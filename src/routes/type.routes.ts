7be7a90a9821e64d9850a661e5992efa20670540
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-02-13 21:10:36 -0400

    mejorar validaciones de proveedor y actualizar lógica de eliminación

--- src/routes/type.routes.ts
+++ src/routes/type.routes.ts
@@ -8,7 +8,7 @@ const typeController = new TypeController();
 const router = Router();
 
 router.get("/", typeController.all); // http://localhost:3900/api/type
-router.get("/:id", typeValidator.validateTypeId, validateFields, typeController.one); // http://localhost:3900/api/type/1
+router.get("/:id", typeValidator.validateTypeId, typeController.one); // http://localhost:3900/api/type/1
 router.post("/", typeValidator.validateType, validateFields, typeController.create); // http://localhost:3900/api/type
 router.put("/:id", typeValidator.validateTypeId, typeValidator.validateType, validateFields, typeController.update); // http://localhost:3900/api/type/1
 router.delete("/:id", typeValidator.validateTypeId, typeController.delete); // http://localhost:3900/api/type/1
