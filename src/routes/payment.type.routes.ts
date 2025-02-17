03bea695ccd0dd3dda53e78da57be249653de3ed
Nikku <yoks.rar@gmail.com>
2025-02-17 17:25:35 -0400

    Creación del yml y arreglo de errores en el crud

--- src/routes/payment.type.routes.ts
+++ src/routes/payment.type.routes.ts
@@ -1,29 +1,28 @@
-import { Router } from "express";
-import { validateFields } from "../middlewares";
-import { PaymentTypeController } from "../controllers";
-import { PaymentTypeValidator } from "../validators";
+import { Router } from "express"
+import { validateFields } from "../middlewares"
+import { PaymentTypeController } from "../controllers"
+import { PaymentTypeValidator } from "../validators"
 
-const router = Router();
-const paymentTypeController = new PaymentTypeController();
-const paymentTypeValidator = new PaymentTypeValidator();
+const router = Router()
+const paymentTypeController = new PaymentTypeController()
+const paymentTypeValidator = new PaymentTypeValidator()
 
-router.get("/", paymentTypeController.all);
-router.get("/:id", paymentTypeValidator.validatePaymentType, paymentTypeController.one); 
-router.post(
-  "/",
-  paymentTypeValidator.validatePaymentType,
-  paymentTypeValidator.validateIfNameIsUse,
-  validateFields,
-  paymentTypeController.create
-);
+//http://localhost:3900/api/PaymentType
+router.get("/", paymentTypeController.all)
+//http://localhost:3900/api/PaymentType/1
+router.get("/:id", paymentTypeValidator.validateIfIdExist, paymentTypeController.one)
+//http://localhost:3900/api/PaymentType
+router.post("/", paymentTypeValidator.validatePaymentTypeCreate, validateFields, paymentTypeController.create)
+//http://localhost:3900/api/PaymentType/1
 router.put(
   "/:id",
   paymentTypeValidator.validatePaymentType,
-  paymentTypeValidator.validatePaymentType,
-  paymentTypeValidator.validateIfNameIsUse, 
-  validateFields, 
-  paymentTypeController.update 
-);
-router.delete("/:id", paymentTypeValidator.validatePaymentType, paymentTypeController.delete); 
+  paymentTypeValidator.validateIfIdExist,
+  paymentTypeValidator.validateIfNameIsUse,
+  validateFields,
+  paymentTypeController.update,
+)
+//http://localhost:3900/api/PaymentType/1
+router.delete("/:id", paymentTypeValidator.validateIfIdExist, paymentTypeController.delete)
 
-export default router;
+export default router
