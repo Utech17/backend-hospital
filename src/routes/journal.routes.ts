b737fab56b5ebb0fbb763c1fe21fc763b68d4649
Utech17 <127623759+Utech17@users.noreply.github.com>
2025-02-20 04:08:21 -0400

    jouernal solo para reporte excel

--- src/routes/journal.routes.ts
+++ src/routes/journal.routes.ts
@@ -1,20 +1,9 @@
 import { Router } from "express";
-import { validateFields } from "../middlewares";
 import { JournalController } from "../controllers";
-import { JournalValidator } from "../validators";
 
-const journalValidator = new JournalValidator();
 const router = Router();
 const journalController = new JournalController();
 
-router.get("/", journalController.all); // http://localhost:3900/api/journal
-router.get("/diary", journalController.getDiaryEntries); // Nueva ruta
-router.get("/balance", journalController.getBalanceSheet); // Nueva ruta
-router.get("/income", journalController.getIncomeStatement);
-router.get("/:id", journalController.one); // http://localhost:3900/api/journal/1
-router.post("/", journalValidator.validateJournal, validateFields, journalController.create); // http://localhost:3900/api/journal
-router.put("/:id", journalValidator.validateJournal, validateFields, journalController.update); // http://localhost:3900/api/journal/1
-router.delete("/:id", journalController.delete); // http://localhost:3900/api/journal/1
 router.post("/excel", journalController.generateExcel); // Para generar el Excel con período
 
 export default router;
